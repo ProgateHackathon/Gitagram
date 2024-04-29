dad1d67c5102de11bf7e97c9285b65ca796c13ce
urassh <shuto.1011@icloud.com>
2024-04-29 19:38:42 +0900

    first commit

---
+++ Gitagram/GitagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitagramApp.swift
+//  Gitagram
+//
+//  Created by 浦山秀斗 on 2024/04/29.
+//
+
+import SwiftUI
+
+@main
+struct GitagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
