3dee73f170de8b4ec8205081015f89c6a57aeb4c
urassh <shuto.1011@icloud.com>
2024-05-01 12:49:11 +0900

    feat: Productのデータモデルを定義

--- Gitagram/Model/DataModel/Developer.swift
+++ Gitagram/Model/DataModel/Developer.swift
@@ -8,11 +8,51 @@
 import Foundation
 
 struct Developer : Identifiable {
-    let id: UUID
-    let name: String
+    typealias DeveloperID = Developer.ID
     
-    init(id: UUID, name: String) {
+    let id: DeveloperID
+    let name: String
+    let gitHubURL: String
+    let imagePath: String
+        
+    init(id: DeveloperID, name: String, gitHubURL: String, imagePath: String) {
         self.id = id
         self.name = name
+        self.gitHubURL = gitHubURL
+        self.imagePath = imagePath
+    }
+    
+    struct ID : Identifiable, Hashable, Codable {
+        let id: UUID
+        
+        var toString: String {
+            return id.uuidString
+        }
+        
+        init(id uuid: UUID) {
+            self.id = uuid
+        }
+    }
+}
+
+extension Developer : Encodable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case link
+        case imagePath
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        do {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(id.id, forKey: .id)
+            try container.encode(name, forKey: .name)
+            try container.encode(gitHubURL, forKey: .link)
+            try container.encode(imagePath, forKey: .imagePath)
+        } catch {
+            print("エンコードできん...!! : \(error)")
+            throw error
+        }
     }
 }
