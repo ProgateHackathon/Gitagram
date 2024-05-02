10f905567a50452eab29e2f40e6df4a019af8c28
urassh <shuto.1011@icloud.com>
2024-05-02 23:02:46 +0900

    fix: urlとimagepathを計算プロパティにした

--- Gitagram/Model/DataModel/Developer.swift
+++ Gitagram/Model/DataModel/Developer.swift
@@ -12,21 +12,21 @@ struct Developer : Identifiable {
     
     let id: DeveloperID
     let name: String
-    let gitHubURL: String
-    let imagePath: String
+    var gitHubURL: String {
+        "https://github.com/\(name)"
+    }
+    var imageURL: String {
+        gitHubURL + ".png"
+    }
         
-    init(id: DeveloperID, name: String, gitHubURL: String, imagePath: String) {
+    init(id: DeveloperID, name: String) {
         self.id = id
         self.name = name
-        self.gitHubURL = gitHubURL
-        self.imagePath = imagePath
     }
     
-    init(name: String, gitHubURL: String, imagePath: String) {
+    init(name: String) {
         self.id = DeveloperID(id: UUID())
         self.name = name
-        self.gitHubURL = gitHubURL
-        self.imagePath = imagePath
     }
     
     struct ID : Identifiable, Hashable, Codable {
@@ -46,8 +46,6 @@ extension Developer : Encodable {
     enum CodingKeys: String, CodingKey {
         case id
         case name
-        case link
-        case imagePath
     }
     
     func encode(to encoder: any Encoder) throws {
@@ -55,8 +53,6 @@ extension Developer : Encodable {
             var container = encoder.container(keyedBy: CodingKeys.self)
             try container.encode(id.id, forKey: .id)
             try container.encode(name, forKey: .name)
-            try container.encode(gitHubURL, forKey: .link)
-            try container.encode(imagePath, forKey: .imagePath)
         } catch {
             print("エンコードできん...!! : \(error)")
             throw error
