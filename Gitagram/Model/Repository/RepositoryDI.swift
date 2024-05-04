2523fc3896f7f4a3cded52ab09a8bac5dc4a6462
urassh <shuto.1011@icloud.com>
2024-05-04 13:30:02 +0900

    feat: repository_diの定義

---
+++ Gitagram/Model/Repository/RepositoryDI.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryDI.swift
+//  Gitagram
+//
+//  Created by 浦山秀斗 on 2024/05/04.
+//
+
+import Foundation
+
+class RepositoryDI {
+    public static let developerClient = DeveloperClient()
+}
