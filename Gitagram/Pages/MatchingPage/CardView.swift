39abfa6bd9de309365156a49c08b37904f4a717c
saki0411 <chipdale411@gmail.com>
2024-05-04 12:08:09 +0900

    add-swipe-like

--- Gitagram/Pages/MatchingPage/CardView.swift
+++ Gitagram/Pages/MatchingPage/CardView.swift
@@ -9,44 +9,56 @@ import SwiftUI
 
 struct CardView: View {
     @State private var xoffset: CGFloat = 0
-    @State private var yoffset: CGFloat = 0
     @State private var degrees: Double = 0
     
     var body: some View {
         ZStack(alignment: .bottom){
             //仮のサンプル画像
-            Rectangle()
-               
-            Image("sampleImage")
-                .resizable()
-                .scaledToFill()
-               
+         
+            ZStack(alignment: .top) {
+             
+                    
+                Image("sampleImage")
+                    .resizable()
+                    .scaledToFill()
+                
+                   
+                SwipeActionIndicatorView(xofset: $xoffset)
+                   
+            }
+                
+            
+           
         }
-        .frame(width: cardWidth,height: cardHeight)
+        .frame(width: SizeConstants.cardWidth,height: SizeConstants.cardHeight)
         .clipShape(RoundedRectangle(cornerRadius: 10))
-        .offset(x: xoffset,y: yoffset)
+        .offset(x: xoffset)
         .rotationEffect(.degrees(degrees))
+        .animation(.snappy,value: xoffset)
         .gesture(
             DragGesture()
-                .onChanged({ value in
-                    withAnimation(.snappy){
-                        xoffset = value.translation.width
-                        degrees = Double(value.translation.width / 25)
-                        yoffset = value.translation.height
-                    }
-                })
+                .onChanged(onDragchanged)
+                .onEnded(onDragEnded)
         )
     }
 }
 
 private extension CardView{
-    private var cardWidth: CGFloat{
-        UIScreen.main.bounds.width - 20
+    func onDragchanged(_ value:  _ChangedGesture<DragGesture>.Value){
+        xoffset = value.translation.width
+        degrees = Double(value.translation.width / 25)
     }
-    private var cardHeight: CGFloat{
-        UIScreen.main.bounds.height/1.45
+    func onDragEnded(_ value:  _ChangedGesture<DragGesture>.Value){
+        let width = value.translation.width
+        
+        if abs(width) <= abs(SizeConstants.screenCutof){
+            xoffset = 0
+            degrees = 0
+        }
     }
 }
+
+
 
 #Preview {
     CardView()
--- Gitagram/View/ContentView.swift
+++ Gitagram/View/ContentView.swift
@@ -8,17 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    var body: some View{
+        Text("hello world")
     }
 }
-
 #Preview {
     ContentView()
 }
