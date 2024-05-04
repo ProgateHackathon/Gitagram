e441c9690d6773b47cf5a340e6213cb2fdb99545
Rino1011 <oy.06ls2@gmail.com>
2024-05-05 00:47:04 +0900

    update-QR

---
+++ Gitagram/View/InputformView/View/InputFormView.swift
@@ -0,0 +1,18 @@
+//
+//  InputFormView.swift
+//  Gitagram
+//
+//  Created by 伊藤璃乃 on 2024/05/04.
+//
+
+import SwiftUI
+
+struct InputFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputFormView()
+}
