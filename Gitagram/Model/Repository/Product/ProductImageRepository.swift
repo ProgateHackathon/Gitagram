5660cba4f7d8184e8d2c2ca21d0bff11765eb900
urassh <shuto.1011@icloud.com>
2024-05-05 00:21:51 +0900

    feaet: repository層まで実装した

---
+++ Gitagram/Model/Repository/Product/ProductImageRepository.swift
@@ -0,0 +1,21 @@
+//
+//  ProductImageRepository.swift
+//  Gitagram
+//
+//  Created by 浦山秀斗 on 2024/05/05.
+//
+
+import Foundation
+import UIKit
+
+class ProductImageRepository : ProductImageRepositoryProtocol {
+    private let client = RepositoryDI.productImageClient
+    
+    func storeImage(id: Product.ID, uiImage: UIImage) {
+        client.uploadImage(product_id: id.toString, image: uiImage)
+    }
+    
+    func fetchImage(id: Product.ID) async -> UIImage? {
+        await client.downloadImage(product_id: id.toString)
+    }
+}
