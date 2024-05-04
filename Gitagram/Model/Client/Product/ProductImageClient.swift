39ab6bb4a8cc96b31bd438ecef48cdd212ad4a21
urassh <shuto.1011@icloud.com>
2024-05-04 23:56:00 +0900

    feat: saveImageの実装をした

---
+++ Gitagram/Model/Client/Product/ProductImageClient.swift
@@ -0,0 +1,33 @@
+//
+//  ProductImageClient.swift
+//  Gitagram
+//
+//  Created by 浦山秀斗 on 2024/05/04.
+//
+
+import Foundation
+import UIKit
+import FirebaseStorage
+
+class ProductImageClient : ProductImageClientProtocol {
+    private let storage = Storage.storage()
+    private let STORAGE_URL = "gs://gitagram-ef516.appspot.com/products"
+    
+    func saveImage(product_id: String, image: UIImage) {
+        let ref = storage.reference(forURL: STORAGE_URL).child(product_id)
+        guard let jpegData = convertToJpegData(uiImage: image) else {
+            print("画像の変換に失敗しました")
+            return
+        }
+        
+        ref.putData(jpegData as Data)
+    }
+    
+    func getImage(product_id: String) -> UIImage? {
+        <#code#>
+    }
+    
+    private func convertToJpegData(uiImage: UIImage) -> NSData? {
+        return uiImage.jpegData(compressionQuality: 1.0) as? NSData
+    }
+}
