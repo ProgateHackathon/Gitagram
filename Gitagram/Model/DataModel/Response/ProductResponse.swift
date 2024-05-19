3a1820d111a28d0c0744fc692aa821a677c999ca
urassh <shuto.1011@icloud.com>
2024-05-19 20:39:58 +0900

    add: ProductResponseを定義した

---
+++ Gitagram/Model/DataModel/Response/ProductResponse.swift
@@ -0,0 +1,28 @@
+//
+//  ProductResponse.swift
+//  Gitagram
+//
+//  Created by 浦山秀斗 on 2024/05/19.
+//
+
+import Foundation
+
+struct ProductResponse : Identifiable, Codable {
+    let id: UUID
+    let title: String
+    let content: String
+    let developerId: UUID
+    let url: String
+    
+    init(from product: Product) {
+        self.id = product.id.toUUID
+        self.title = product.title
+        self.content = product.content
+        self.developerId = product.developerId.toUUID
+        self.url = product.url
+    }
+    
+    func toProduct() -> Product {
+        return Product(id: Product.ID(id: id), title: title, content: content, developerId: Developer.ID(id: developerId), url: url)
+    }
+}
