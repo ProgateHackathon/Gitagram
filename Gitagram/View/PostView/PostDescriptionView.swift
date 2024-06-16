f8159e3073accb5b468b931904243b7d385affbc
urassh <shuto.1011@icloud.com>
2024-06-16 11:12:24 +0900

    refactor: postDescriptionView

--- Gitagram/View/PostView/PostDescriptionView.swift
+++ Gitagram/View/PostView/PostDescriptionView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct PostDescriptionView: View {
-    @State var discription = ""
-    @Binding var title : String
+    @State var cardData: CardDataModel
+    @State var description = ""
     
     var body: some View {
         VStack{
             ProgressView("", value: 0.4)
                 .tint(Color.pink)
-            
                 .cornerRadius(8)
                 .scaleEffect(1.3)
                 .padding(.bottom, 20)
@@ -26,14 +25,20 @@ struct PostDescriptionView: View {
                 .font(.system(size: 30, weight: .black, design: .default))
                 .padding(.bottom,30)
             
-            
-            TextField("リポジトリの説明を入力してね", text: $discription)
+            TextField("リポジトリの説明を入力してね", text: $description)
                 .frame(alignment: .leading)
                 .padding(.leading,10)
+                .onChange(of: description) {
+                    let product = cardData.product.setContent(from: description)
+                    cardData = cardData.setProduct(from: product)
+                }
+            
             Divider()
+            
             Spacer()
+            
             NavigationLink{
-                PostURLView(title: $title, discription: $discription)
+                PostURLView(cardData: cardData)
             } label: {
                 Text("次へ")
                     .padding(.horizontal,120)
@@ -42,17 +47,8 @@ struct PostDescriptionView: View {
                     .foregroundColor(.white)
                     .background(Color(Color(red: 0.82, green: 0.6, blue: 0.97)))
                     .cornerRadius(30)
-                   
             }
         }
-      
-        
     }
     
-}
-
-
-
-#Preview {
-    PostDescriptionView(title: .constant(""))
 }
