5ca425f201900348302069b9a1d709a39d5465d4
urassh <shuto.1011@icloud.com>
2024-06-16 12:08:09 +0900

    refactor: PostImageView.swift

--- Gitagram/View/PostView/PostImageView.swift
+++ Gitagram/View/PostView/PostImageView.swift
@@ -9,21 +9,15 @@ import SwiftUI
 import PhotosUI
 
 struct PostImageView: View {
-    
-    @State var developer = Developer(githubId: "")
+    @State var cardData: CardDataModel
     @State var showImagePicker = false
-    @State var image: UIImage?
-    @State private var selectedPhoto: PhotosPickerItem?
-    @Binding var title: String
-    @Binding var discription: String
-    @Binding var url: String
+    @State var selectedPhoto: PhotosPickerItem?
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         VStack{
             ProgressView("", value: 1)
                 .tint(Color.pink)
-            
                 .cornerRadius(8)
                 .scaleEffect(1.3)
                 .padding(.bottom, 20)
@@ -31,46 +25,46 @@ struct PostImageView: View {
             Text("リポジトリの画像を貼ろう")
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.leading,10)
-            
                 .font(.system(size: 30, weight: .black, design: .default))
                 .padding(.bottom,30)
+            
             Spacer()
+            
             PhotosPicker(selection: $selectedPhoto,matching: .images){
-                
                 HStack{
                     Image(systemName: "photo.badge.plus")
                     Text("画像のアップロード")
                 }
                 .padding(.horizontal,40)
                 .padding(.vertical,15)
-                
                 .foregroundColor(.black)
                 .background(Color.white)
-                
                 .cornerRadius(35)
                 .overlay(
                     RoundedRectangle(cornerRadius: 30)
                         .stroke(Color(Color(red: 0.82, green: 0.6, blue: 0.97)), lineWidth: 3)
                 )
             }
-            .onChange(of: selectedPhoto) { selectedPhoto in
-                Task { await loadImageFromSelectedPhoto(photo: selectedPhoto) }
+            .onChange(of: selectedPhoto) {
+                Task {
+                    guard let unwrap = selectedPhoto else { return }
+                    let loadedImage = await loadImageFromSelectedPhoto(photo: unwrap)
+                    cardData = cardData.setImage(from: loadedImage ?? cardData.productImage)
+                }
             }
-            
             .padding(.bottom,20)
             
             Button(action: {
-                Task{
-                    do{
-                        await PostProductUseCase().execute(product: Product(title: title, content: discription, developerId: developer.id, url: url), productImage: (image ?? UIImage(named: "back"))!
-                        )}
+                Task {
+                    if cardData.isComplete() {
+                        await PostProductUseCase().execute(product: cardData.product, productImage: cardData.productImage)
+                    }
                 }
                 
                 UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
                 
             }, label: {
                 Text("保存")
-                
                     .padding(.horizontal,120)
                     .padding(.vertical,15)
                     .font(.system(size: 10, weight: .medium, design: .default))
@@ -81,27 +75,13 @@ struct PostImageView: View {
                 
             })
         }
-        
-        .onAppear(){
-            Task{
-                do{
-                    
-                    developer = await GetLoginDeveloperUseCase().execute()!
-                }
-                
-            }
-            
-        }
-        
-    }
-    private func loadImageFromSelectedPhoto(photo: PhotosPickerItem?) async {
-        if let data = try? await photo?.loadTransferable(type: Data.self) {
-            self.image = UIImage(data: data)
-        }
     }
     
-}
-
-#Preview {
-    PostImageView(developer: Developer(githubId: "am2525nyan"), title: .constant(""), discription: .constant(""), url: .constant(""))
+    private func loadImageFromSelectedPhoto(photo: PhotosPickerItem) async -> UIImage? {
+        if let data = try? await photo.loadTransferable(type: Data.self) {
+            return UIImage(data: data)
+        }
+        
+        return nil
+    }
 }
