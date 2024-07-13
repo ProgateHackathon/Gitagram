aba334387d338fbdd925d94c22f360050cdccca5
saki0411 <chipdale411@gmail.com>
2024-07-13 13:10:23 +0900

    fix

--- Gitagram/View/MatchingView/MatchingViewModel.swift
+++ Gitagram/View/MatchingView/MatchingViewModel.swift
@@ -14,10 +14,10 @@ class MatchingViewModel: ObservableObject {
     @Published var isLoading = true
     @Published var repositories: [CardData] = []
     @Published var buttonSwipeAction: SwipeAction?
-
+    
     func removeCard(_  product: Product){
         Task{
-           try await Task.sleep(nanoseconds: 500_000_000)
+            try await Task.sleep(nanoseconds: 500_000_000)
             guard let index = repositories.firstIndex(where: { $0.product.id == product.id }) else { return }
             repositories.remove(at: index)
         }
@@ -40,35 +40,19 @@ class MatchingViewModel: ObservableObject {
     func fetchCardInformation() async -> [CardData] {
         var cardList = [CardData]()
         let products = await GetProductListUseCase().execute()
-
+        
         for product in products {
             async let developer = GetDeveloperUseCase().execute(id: product.developer.id)
-            async let productURL = downloadImageURL(product_id: product.id.toString)
+            async let productURL = GetProductImageUseCase().urlexecute(id: product.id)
             
             if let developer = await developer, let productURL = await productURL {
                 let cardData = CardData(product: product, loginHost: developer, url: productURL)
                 cardList.append(cardData)
-
+                
             }
         }
-
+        
         return cardList
     }
-    func downloadImageURL(product_id: String) async -> URL? {
-      let STORAGE_URL = "gs://gitagram-ef516.appspot.com/products"
- let storage = Storage.storage()
-      
-            let ref = storage.reference(forURL: STORAGE_URL).child(product_id)
-            
-            do {
-                let url = try await ref.downloadURL()
-                print("できたよ")
-                return url
-            } catch {
-                print("Error downloading image URL: \(error.localizedDescription)")
-                return nil
-            }
-        }
-
-
+    
 }
